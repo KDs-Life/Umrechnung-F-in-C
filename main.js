71ad9483ab9af97bbe95a8b2609ff45f76a2c248
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2024-02-02 15:35:09 +0100

    html und js erstellt

---
+++ main.js
@@ -0,0 +1,62 @@
+var ENVIRONMENT_NAME = "Ambient Temperature";
+var METRIC;
+var unit;
+
+function setup() {
+  IoEClient.setup({
+    type: "Temperature Monitor",
+    states: [
+      {
+        name: "Temperature",
+        type: "number",
+        unit: "&deg;C",
+        imperialUnit: "&deg;F",
+        toImperialConversion: "x*1.8+32",
+        toMetricConversion: "(x-32)/1.8",
+        decimalDigits: 1,
+      },
+    ],
+  });
+
+  measurementSystemChangeEvent();
+}
+
+function loop() {
+  detect();
+  delay(1000);
+}
+
+function measurementSystemChangeEvent() {
+  METRIC = isUsingMetric();
+  unit = METRIC ? "C" : "F";
+  detect();
+}
+
+function detect() {
+  var value = Environment.get(ENVIRONMENT_NAME);
+
+  setCustomText(30, 25, 200, 20, convertTemperature(value) + "F");
+
+  IoEClient.reportStates(value);
+  setDeviceProperty(getName(), "level", value);
+}
+
+function convertTemperature(value) {
+  if (METRIC) return value.toFixed(2);
+  else return (value * 1.8 + 32).toFixed(2);
+}
+
+//HIER WURDE NUR EIN CODE HINZUGGEFÜGT; DER REST IST VON DER VORHERIGEN AUFGABE: DIESER CODE IST FÜR DIE UMRECHNUNG VON FAHRENHEIT IN CELSIUS
+
+function convertToFahrenheit() {
+  const fahrenheitValue = parseFloat(
+    //parseFloat wandelt den String in eine Zahl um. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat?retiredLocale=de
+    // nutzen const, weil sich der Wert nicht verändern wird.
+    document.getElementById("fahrenheitInput").value
+  );
+  let celsiusValue = (fahrenheitValue - 32) / 1.8; //Umrechnung von Fahrenheit in Celsius. Die Formel ist: (F - 32) / 1.8.
+  //nutzen let, weil sich der Wert verändern wird.
+  document.getElementById("celsiusOutput").textContent = //celsiusOutput ist das Element, in dem das Ergebnis angezeigt wird. siehe im html-Code.
+    celsiusValue.toFixed(2); //toFixed(2) rundet die Zahl auf 2 Nachkommastellen. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed?retiredLocale=de
+  //(2) zwei nachkommerstellen
+}
